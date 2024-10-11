6fae9914bc57c41a963972c7d12a826603e7a852
Shields321 <106932598+Shields321@users.noreply.github.com>
2024-10-10 23:28:58 -0400

    added a script for enemy spawns

--- Assets/scripts/Spawn.cs
+++ Assets/scripts/Spawn.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawn : MonoBehaviour
+namespace Game
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Spawn : MonoBehaviour
     {
-        
-    }
+        public GameObject Entity;
+        public Transform player;
+        public float spawnRate;
+        private float timer = 0f;
+        private float spawnRadius = 5f;
+        void Start()
+        {
+            player = transform;
+            spawnEntity();
+        }
+        void Update()
+        {
+            timer += Time.deltaTime;
+            if (timer >= spawnRate)
+            {
+                spawnEntity();
+                timer = 0f;
+            }
+        }
+        public void spawnEntity()
+        {            
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            Debug.Log("spawning Entity");
+            Vector2 offset = Random.insideUnitSphere * spawnRadius;
+            Vector2 spawnPos = (Vector2)player.position + offset;
+            
+            Instantiate(Entity, spawnPos, Quaternion.identity);
+        }
+
     }
 }
